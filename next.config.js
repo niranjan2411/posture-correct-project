f566047ba504aee09a4525b7f3b2e9cff5a7a00b
Niranjan Raj <niranjanrajsoni6793@gmail.com>
2025-10-04 22:50:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Set this back to true
   reactStrictMode: true,
+  
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
